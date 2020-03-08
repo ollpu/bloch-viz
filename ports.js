95aa33852ac334307586d548878005ed0819d25c
Roope Salmi <rpsalmi@gmail.com>
2020-03-08 23:42:51 +0200

    Display rotating axis nicely

--- ports.js
+++ ports.js
@@ -12,22 +12,12 @@ var ports = {
 function Port(expr) {
   this.mat = math.evaluate(expr);
   this.eigenvector = eigenvec(this.mat);
-  console.log(math.format(this.eigenvector));
   [this.to_eigenbasis, this.from_eigenbasis] = eigenbasis(this.eigenvector);
   var v = [[1/math.sqrt(2)], [1/math.sqrt(2)]];
   v = math.multiply(this.from_eigenbasis, this.mat, this.to_eigenbasis, v);
   var [[a], [z]] = v._data;
   z = math.divide(z, a);
   this.rotation = math.arg(z);
-  console.log(this.rotation);
-  
-  // 0 <= t <= 1
-  this.animatedRotation = function(v, t) {
-    v = math.multiply(this.from_eigenbasis, v);
-    v._data[1][0] = math.multiply(v._data[1][0], math.complex({r: 1, phi: t*this.rotation}));
-    v = math.multiply(this.to_eigenbasis, v);
-    return v;
-  }
 }
 
 
